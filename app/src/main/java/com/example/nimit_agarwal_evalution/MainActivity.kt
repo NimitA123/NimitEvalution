cb94a762bd2f863749f343b878704990792e87ef
NimitAgra <89835196+NimitAgra@users.noreply.github.com>
2021-09-27 13:24:39 +0530

    NimitEvalution

---
+++ app/src/main/java/com/example/nimit_agarwal_evalution/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nimit_agarwal_evalution
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
