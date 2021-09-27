5d94e8fcb11aa28cbc367ad05cc25968a28da3a1
NimitAgra <89835196+NimitAgra@users.noreply.github.com>
2021-09-27 15:50:57 +0530

    NimitEvalution

--- app/src/main/java/com/example/nimit_agarwal_evalution/MusicViewHolder.kt
+++ app/src/main/java/com/example/nimit_agarwal_evalution/MusicViewHolder.kt
@@ -2,11 +2,12 @@ package com.example.nimit_agarwal_evalution
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_layout.view.*
 
-class MusicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class MusicViewHolder(private  val view: View): RecyclerView.ViewHolder(view) {
     fun setData(resultsDTO: ResultsDTO){
-        view.tvName.text = responseDTO.name //.text means setText
-        view.tvEmail.text = responseDTO.email //.text means setText
-        view.tvBody.text = responseDTO.body //.text means setText
+        view.textView.text = resultsDTO.trackName.toString() //.text means setText
+        view.textView2.text = resultsDTO.artistName.toString() //.text means setText
+      //  view.tvBody.text = responseDTO.body //.text means setText
     }
 }
